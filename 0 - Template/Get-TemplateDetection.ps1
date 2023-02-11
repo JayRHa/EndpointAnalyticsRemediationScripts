18881a6eb80b9b500d2017f297547b0effe0999d
Jannik Reinhard <Jannik.Reinhard@outlook.de>
2023-02-11 11:23:00 +0100

    Init Template

---
+++ 0 - Template/Get-TemplateDetection.ps1
@@ -0,0 +1,11 @@
+<#
+Version: 1.0
+Author: 
+- Joey Verlinden (https://www.joeyverlinden.com/)
+- Andrew Taylor (https://andrewstaylor.com/)
+- Jannik Reinhard (jannikreinhard.com)
+Script: Get-TemplateDetection
+Description:
+Release notes:
+Version 1.0: Init
+#> 
---
+++ 0 - Template/Get-TemplateRemedaiton.ps1
@@ -0,0 +1,11 @@
+<#
+Version: 1.0
+Author: 
+- Joey Verlinden (https://www.joeyverlinden.com/)
+- Andrew Taylor (https://andrewstaylor.com/)
+- Jannik Reinhard (jannikreinhard.com)
+Script: Get-TemplateRemediation
+Description:
+Release notes:
+Version 1.0: Init
+#> 
