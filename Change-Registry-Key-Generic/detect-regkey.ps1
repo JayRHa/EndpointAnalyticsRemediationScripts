5b1350d8d07ba6dc97ff77c5b877497e44299244
andrew-s-taylor <andrew@tinymice.co.uk>
2023-03-08 17:07:27 +0000

    Generic registry check

---
+++ Change-Registry-Key-Generic/detect-regkey.ps1
@@ -0,0 +1,35 @@
+<#
+Version: 1.0
+Author: 
+- Joey Verlinden (https://www.joeyverlinden.com/)
+- Andrew Taylor (https://andrewstaylor.com/)
+- Jannik Reinhard (jannikreinhard.com)
+Script: detect-fastboot.ps1
+Description: Detects if Fastboot is enabled
+Release notes:
+Version 1.0: Init
+#> 
+
+##Enter the path to the registry key for example HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+$regpath = ""
+
+##Enter the name of the registry key for example EnableLUA
+$regname = ""
+
+##Enter the value of the registry key we are checking for, for example 0
+$regvalue = ""
+
+
+Try {
+    $Registry = Get-ItemProperty -Path $regpath -Name $regname -ErrorAction Stop | Select-Object -ExpandProperty $regname
+    If ($Registry -eq $regvalue){
+        Write-Output "Compliant"
+        Exit 0
+    } 
+    Write-Warning "Not Compliant"
+    Exit 1
+} 
+Catch {
+    Write-Warning "Not Compliant"
+    Exit 1
+}
---
+++ Change-Registry-Key-Generic/remediate-regkey.ps1
@@ -0,0 +1,26 @@
+<#
+Version: 1.0
+Author: 
+- Joey Verlinden (https://www.joeyverlinden.com/)
+- Andrew Taylor (https://andrewstaylor.com/)
+- Jannik Reinhard (jannikreinhard.com)
+Script: remediate-fastboot.ps1
+Description: Disables Fastboot via registry key
+Release notes:
+Version 1.0: Init
+#> 
+
+##Enter the path to the registry key for example HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System
+$regpath = ""
+
+##Enter the name of the registry key for example EnableLUA
+$regname = ""
+
+##Enter the value of the registry key for example 0
+$regvalue = ""
+
+##Enter the type of the registry key for example DWord
+$regtype = ""
+
+
+New-ItemProperty -LiteralPath $regpath -Name $regname -Value $regvalue -PropertyType $regtype -Force -ea SilentlyContinue;
