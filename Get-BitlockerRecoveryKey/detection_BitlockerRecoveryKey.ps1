3788d8f2e2f0cc7b203aadbc96bcaefc6e4308fe
FlorianSLZ <38555854+FlorianSLZ@users.noreply.github.com>
2023-03-08 07:09:11 +0100

    Get BitLocker Recovery Key
    Obtain the Bitlocker recovery key for a subset of devices, as scope tag permissions do not support recovery keys.

---
+++ Get-BitlockerRecoveryKey/detection_BitlockerRecoveryKey.ps1
@@ -0,0 +1,33 @@
+<#
+Version: 1.0
+Author: 
+- Joey Verlinden (joeyverlinden.com)
+- Andrew Taylor (andrewstaylor.com)
+- Florian Slazmann (scloud.work)
+- Jannik Reinhard (jannikreinhard.com)
+Script: detection_BitlockerRecoveryKey
+Description:
+Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
+Version 1.0: Init
+#> 
+
+Try {
+$Result = (Get-BitLockerVolume -MountPoint C).KeyProtector
+$Recoverykey = $result.recoverypassword
+
+If ($recoverykey -ne $null)
+{
+    Write-Output "Bitlocker recovery key available $Recoverykey "
+    exit 0
+}
+Else
+{
+    Write-Output "No bitlocker recovery key available starting remediation"
+    exit 1
+}
+}
+catch
+{
+Write-Warning "Value Missing"
+exit 1
+}
