2a9928332d534cb229b653078e8cba0f58df5189
andrew-s-taylor <andrew@tinymice.co.uk>
2023-03-08 17:27:59 +0000

    Reboot notification script

---
+++ Toast-RebootMessage/detect-reboot.ps1
@@ -0,0 +1,37 @@
+<#
+Version: 1.0
+Author: 
+- Joey Verlinden (joeyverlinden.com)
+- Andrew Taylor (andrewstaylor.com)
+- Florian Slazmann (scloud.work)
+- Jannik Reinhard (jannikreinhard.com)
+Script: detect-fastboot.ps1
+Description: Detects if Fastboot is enabled
+Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
+Version 1.0: Init
+#> 
+
+##Check how long machine has been on for
+$now = Get-Date -UFormat "%s" -Date (Get-Date)
+$poweron = (Get-Date -UFormat "%s" -Date (Get-Process -Id $pid).StartTime)
+
+##Check the difference between the two
+$diff = $now - $poweron
+
+##Convert that to hours
+$hours = $diff / 3600
+
+##How long shall we compare against in days??
+$thresholddays = 7
+
+##Convert that to hours
+$thresholdhours = $thresholddays * 24
+
+if ($hours -gt $thresholdhours) {
+    write-host "Machine has been on for more than $thresholddays days"
+    exit 0
+}
+else {
+    write-host "Machine has been on for less than $thresholddays days"
+    exit 1
+}
---
+++ Toast-RebootMessage/remediate-reboot.ps1
@@ -0,0 +1,42 @@
+<#
+Version: 1.0
+Author: 
+- Joey Verlinden (joeyverlinden.com)
+- Andrew Taylor (andrewstaylor.com)
+- Florian Slazmann (scloud.work)
+- Jannik Reinhard (jannikreinhard.com)
+Script: remediate-fastboot.ps1
+Description: Disables Fastboot via registry key
+Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
+Version 1.0: Init
+#> 
+
+function Show-Notification {
+    [cmdletbinding()]
+    Param (
+        [string]
+        $ToastTitle,
+        [string]
+        [parameter(ValueFromPipeline)]
+        $ToastText
+    )
+
+    [Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null
+    $Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02)
+
+    $RawXml = [xml] $Template.GetXml()
+    ($RawXml.toast.visual.binding.text|where {$_.id -eq "1"}).AppendChild($RawXml.CreateTextNode($ToastTitle)) > $null
+    ($RawXml.toast.visual.binding.text|where {$_.id -eq "2"}).AppendChild($RawXml.CreateTextNode($ToastText)) > $null
+
+    $SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument
+    $SerializedXml.LoadXml($RawXml.OuterXml)
+
+    $Toast = [Windows.UI.Notifications.ToastNotification]::new($SerializedXml)
+    $Toast.Tag = "PowerShell"
+    $Toast.Group = "PowerShell"
+    $Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(1)
+
+    $Notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell")
+    $Notifier.Show($Toast);
+}
+Show-Notification -ToastTitle "Please Restart your Machine" -ToastText "Your computer has been on for more than 7 days, please reboot when possible"
