aff30e1b3d269374ef71f61d78b2688e7ff90e70
Andrew Taylor <59832110+andrew-s-taylor@users.noreply.github.com>
2023-07-03 18:46:06 +0100

    Update Uninstall-PrivateTeamsRemedaiton.ps1

--- Uninstall-PrivateTeams/Uninstall-PrivateTeamsRemedaiton.ps1
+++ Uninstall-PrivateTeams/Uninstall-PrivateTeamsRemedaiton.ps1
@@ -14,7 +14,7 @@ Context: 64 Bit
 #> 
 
 try{
-    Get-AppxPackage -Name MicrosoftTeams | Remove-AppxPackage -ErrorAction stop
+    Get-AppxPackage -Name MicrosoftTeams -allusers | Remove-AppxPackage -ErrorAction stop
     Write-Host "Private MS Teams app successfully removed"
 }catch{
     Write-Error "Error removing Microsoft Teams app"
