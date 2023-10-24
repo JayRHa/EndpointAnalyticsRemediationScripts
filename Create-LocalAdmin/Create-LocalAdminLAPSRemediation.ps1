8ded89e06bc64b601e2181195f3c596355984e46
Simon Skotheimsvik <57434443+SimonSkotheimsvik@users.noreply.github.com>
2023-10-24 08:55:19 +0200

    Update Create-LocalAdminLAPSRemediation.ps1
    Modified to handle local admin group named other things than Administrators.

--- Create-LocalAdmin/Create-LocalAdminLAPSRemediation.ps1
+++ Create-LocalAdmin/Create-LocalAdminLAPSRemediation.ps1
@@ -7,7 +7,7 @@ Author:
 - Jannik Reinhard (jannikreinhard.com)
 - Simon Skotheimsvik (skotheimsvik.no)
 Script: Create-LocalAdmin
-Description: Add a local admin with randomized password, ensuring that we do not have an account with a static password across all devices before Windows LAPS takes effect.
+Description: Add a local admin with a randomized password, ensuring that we do not have an account with a static password across all devices before Windows LAPS takes effect.
 Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
 Version 1.1: Init
 Run as: Admin
@@ -16,6 +16,7 @@ Context: 64 Bit
 
 $localAdminName = ""
 $password = -join ((65..90) + (97..122) + (48..57) + (35..38) + (40..47) | Get-Random -Count 35 | ForEach-Object {[char]$_}) | ConvertTo-SecureString -AsPlainText -Force
+$Localadmingroupname = $((Get-LocalGroup -SID "S-1-5-32-544").Name)
 
 New-LocalUser "$localAdminName" -Password $password -FullName "$localAdminName" -Description "LAPS account"
-Add-LocalGroupMember -Group "Administrators" -Member "$localAdminName"
+Add-LocalGroupMember -Group $Localadmingroupname -Member "$localAdminName"
