1c5338a7929a7db647e9fe7dab726253ff87fd1e
Jose Schenardie <schenardie@gmail.com>
2024-02-12 11:32:21 +1100

    SOftDist

--- Reset-SoftwareDistributionFolder/Remediate-Reset-SoftwareDistributionFolder.ps1
+++ Reset-SoftwareDistributionFolder/Remediate-Reset-SoftwareDistributionFolder.ps1
@@ -2,7 +2,7 @@
 Version: 1.0
 Author: 
 - Jose Schenardie (intune.tech)
-Script: Reset-SoftwareDistributionFolder
+Script: Remediate-Reset-SoftwareDistributionFolder
 Description: Script to reset the SoftwareDistribution folder by stopping Windows Updates services, renaming the folder to SoftwareDistribution.old and starting the services again.
 Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
 Version 1.0: Init
